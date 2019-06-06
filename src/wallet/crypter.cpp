41704ab600f0ccabbed952b7717498641c82c663
Andrew Chow <achow101-github@achow101.com>
2019-06-06 23:58:21 +0200

    Remove CCryptoKeyStore and move all of it's functionality into CWallet
    Instead of having a separate CCryptoKeyStore that handles the encryption
    stuff, just roll it all into CWallet.

--- src/wallet/crypter.cpp
+++ src/wallet/crypter.cpp
@@ -110,8 +110,7 @@ bool CCrypter::Decrypt(const std::vector<unsigned char>& vchCiphertext, CKeyingM
     return true;
 }
 
-
-static bool EncryptSecret(const CKeyingMaterial& vMasterKey, const CKeyingMaterial &vchPlaintext, const uint256& nIV, std::vector<unsigned char> &vchCiphertext)
+bool EncryptSecret(const CKeyingMaterial& vMasterKey, const CKeyingMaterial &vchPlaintext, const uint256& nIV, std::vector<unsigned char> &vchCiphertext)
 {
     CCrypter cKeyCrypter;
     std::vector<unsigned char> chIV(WALLET_CRYPTO_IV_SIZE);
