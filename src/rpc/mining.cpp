a1ff8178ae4737c44130ceb1b9ac1d5e2304c755
Patrick Strateman <patrick.strateman@gmail.com>
2019-06-21 13:27:00 -0400

    rpc: Allow shutdown while in generateblocks
    By checking the shutdown flag every loop we can use the entire nonce space
    instead of breaking every 16 bits to check the shutdown flag.
    
    This has been possible since the shutdown flag was switched to an atomic,
    before that change it was controlled by a condition variable and lock.

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -110,7 +110,6 @@ static UniValue getnetworkhashps(const JSONRPCRequest& request)
 
 static UniValue generateBlocks(const CScript& coinbase_script, int nGenerate, uint64_t nMaxTries)
 {
-    static const int nInnerLoopCount = 0x10000;
     int nHeightEnd = 0;
     int nHeight = 0;
 
@@ -131,14 +130,14 @@ static UniValue generateBlocks(const CScript& coinbase_script, int nGenerate, ui
             LOCK(cs_main);
             IncrementExtraNonce(pblock, ::ChainActive().Tip(), nExtraNonce);
         }
-        while (nMaxTries > 0 && pblock->nNonce < nInnerLoopCount && !CheckProofOfWork(pblock->GetPoWHash(), pblock->nBits, true, Params().GetConsensus())) {
+        while (nMaxTries > 0 && pblock->nNonce < std::numeric_limits<uint32_t>::max() && !CheckProofOfWork(pblock->GetPoWHash(), pblock->nBits, true, Params().GetConsensus())) {
             ++pblock->nNonce;
             --nMaxTries;
         }
-        if (nMaxTries == 0) {
+        if (nMaxTries == 0 || ShutdownRequested()) {
             break;
         }
-        if (pblock->nNonce == nInnerLoopCount) {
+        if (pblock->nNonce == std::numeric_limits<uint32_t>::max()) {
             continue;
         }
         std::shared_ptr<const CBlock> shared_pblock = std::make_shared<const CBlock>(*pblock);
