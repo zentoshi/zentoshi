878bf73771a94fbb6e27bbc6a19612635e455f3f
MarcoFalke <falke.marco@gmail.com>
2019-06-26 10:28:13 -0400

    scripted-diff: Avoid passing PACKAGE_NAME for translation
    -BEGIN VERIFY SCRIPT-
    sed -i --regexp-extended -e 's/\<\w+(::\w+)?\(PACKAGE_NAME\)/PACKAGE_NAME/g' $(git grep -l --extended-regexp '\<\w+(::\w+)?\(PACKAGE_NAME\)' src)
    -END VERIFY SCRIPT-

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -36,6 +36,8 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
     devicePixelRatio = static_cast<QGuiApplication*>(QCoreApplication::instance())->devicePixelRatio();
 
     // define text to place
+    QString titleText       = PACKAGE_NAME;
+    QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion()));
     QString copyrightText   = QString::fromUtf8(CopyrightHolders(strprintf("\xc2\xA9 %u ", COPYRIGHT_YEAR)).c_str());
     QString titleAddText    = networkStyle->getTitleAddText();
     QString font            = QApplication::font().toString();
