cce623b70d98a1b99ed90f796deb4c20abf4f523
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-07-07 06:20:19 +0300

    Remove unused guard
    It is no longer used since switching to autotools build system.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -423,7 +423,6 @@ static void SetupUIArgs()
     gArgs.AddArg("-uiplatform", strprintf("Select platform to customize UI for (one of windows, macosx, other; default: %s)", BitcoinGUI::DEFAULT_UIPLATFORM), true, OptionsCategory::GUI);
 }
 
-#ifndef BITCOIN_QT_TEST
 int GuiMain(int argc, char* argv[])
 {
 #ifdef WIN32
@@ -608,4 +607,3 @@ int GuiMain(int argc, char* argv[])
     }
     return rv;
 }
-#endif // BITCOIN_QT_TEST
