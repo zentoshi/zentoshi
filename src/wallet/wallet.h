9ff53374fcec5538d0a649c30554a10c08176b8a
Antoine Riard <ariard@student.42.fr>
2019-08-22 13:16:40 -0400

    Modify wallet tx status if has been reorged out
    Add a LockChain method to CWallet to know if we can lock or query
    chain state safely.
    
    At tx loading, we rely on chain to know if hashBlock of tx is still
    in main chain. If not, we set its status to unconfirmed and reset
    its hashBlock/nIndex.
    
    If wallet loaded is the wallet-tool one, all wallet txn will
    show up with a height of zero. It doesn't matter as status is not
    used by wallet-tool.
    
    We take lock prematurely in CWallet::LoadWallet and CWallet::Verify
    to ensure that lock order is respected between cs_main an cs_wallet.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1080,6 +1080,9 @@ public:
     bool IsLocked() const;
     bool Lock();
 
+    /** Interface to assert chain access and if successful lock it */
+    std::unique_ptr<interfaces::Chain::Lock> LockChain() { return m_chain ? m_chain->lock() : nullptr; }
+
     std::map<uint256, CWalletTx> mapWallet GUARDED_BY(cs_wallet);
 
     typedef std::multimap<int64_t, CWalletTx*> TxItems;
@@ -1249,7 +1252,7 @@ public:
 
     void MarkDirty();
     bool AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose=true);
-    void LoadToWallet(const CWalletTx& wtxIn) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
+    void LoadToWallet(CWalletTx& wtxIn) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
     void TransactionAddedToMempool(const CTransactionRef& tx) override;
     void BlockConnected(const CBlock& block, const std::vector<CTransactionRef>& vtxConflicted) override;
     void BlockDisconnected(const CBlock& block) override;
